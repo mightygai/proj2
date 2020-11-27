4c342e3d2ec7423cc0f005a5174ee27ff0ca75d9
Matthew Drummond-Stoyles <mcdrummo@ryerson.ca>
2020-11-27 18:15:20 -0500

    Fixed c++ files

--- Project2Robot.cpp
+++ Project2Robot.cpp
@@ -1,77 +1,60 @@
-include <Servo.h>
-include <Stepper.h>
+#include <Servo.h>
+#include <Stepper.h>
+#include <Arduino.h>
 
-#define PIN_SERVO             12
-#define PIN_TRIG              04
-#define PIN_ECHO              05
-#define PIN_STEPPER_1         08
-#define PIN_STEPPER_2         09
-#define PIN_STEPPER_3         10
-#define PIN_STEPPER_4         11
+#include "Project2Robot.h"
 
-#define STEPS_PER_REV         32
-
-using namespace proj2;
-
-class Project2Robot
+Project2Robot::Project2Robot()
 {
-  public:
-    double steeringAngle;
-    Servo servo;
-    Stepper stepper;
+  stepper.setSpeed(700);
 
-    Project2Robot()
-    {
-      servo.attach(PIN_SERVO);
-      stepper = Stepper(
-        STEPS_PER_REV,
-        PIN_STEPPER_1,
-        PIN_STEPPER_3,
-        PIN_STEPPER_2,
-        PIN_STEPPER_4
-        );
-        stepper.setSpeed(700);
-    }
-    
-    void setSteeringAngle(double newSteeringAngle)
-    {
-      if (isSteeringAngleOK(newSteeringAngle))
-      {
-        steeringAngle = newSteeringAngle;
-        servo.write(newSteeringAngle);
-      }
-    }
+  pinMode(PIN_TRIG, OUTPUT);
+  pinMode(PIN_ECHO, INPUT);
+}
 
-    void adjustSteeringAngle(double steeringAngleAdjustment)
-    {
-      setSteeringAngle(steeringAngle + steeringAngleAdjustment);
-    }
+void Project2Robot::initializeServo()
+{
+  servo.attach(PIN_SERVO);
+}
 
-    bool isSteeringAngleOK(double newSteeringAngle)
-    {
-      return newSteeringAngle >= 60 || newSteeringAngle <= 120;
-    }
+void Project2Robot::setSteeringAngle(double newSteeringAngle)
+{
+  if (isSteeringAngleOK(newSteeringAngle))
+  {
+    steeringAngle = newSteeringAngle;
+    servo.write(newSteeringAngle);
+  }
+}
 
-    void moveForwardOneStep()
-    {
-      stepper.step(1);
-    }
+void Project2Robot::adjustSteeringAngle(double steeringAngleAdjustment)
+{
+  setSteeringAngle(steeringAngle + steeringAngleAdjustment);
+}
 
-    void moveBackwardOneStep()
-    {
-      stepper.step(-1);
-    }
+bool Project2Robot::isSteeringAngleOK(double newSteeringAngle)
+{
+  return newSteeringAngle >= 60 && newSteeringAngle <= 120;
+}
 
-    double readForwardDistance()
-    {
-      digitalWrite(PIN_TRIG, LOW);
-      delayMicroseconds(2);
-      digitalWrite(PIN_TRIG, HIGH);
-      delayMicroseconds(10);
-      digitalWrite(PIN_TRIG, LOW);
-      
-      duration = pulseIn(PIN_ECHO, HIGH);
-      distance = 100.0*(343.0*(duration/2.0))/1000000.0;
-      return distance;
-    }
+void Project2Robot::moveForwardOneStep()
+{
+  stepper.step(1);
+}
+
+void Project2Robot::moveBackwardOneStep()
+{
+  stepper.step(-1);
+}
+
+double Project2Robot::readForwardDistance()
+{
+  digitalWrite(PIN_TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(PIN_TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(PIN_TRIG, LOW);
+  
+  double duration = pulseIn(PIN_ECHO, HIGH);
+  double distance = 100.0*(343.0*(duration/2.0))/1000000.0;
+  return distance;
 }
